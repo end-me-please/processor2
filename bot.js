d6d751749a64cc0ae3d8ac9e500b877fb01562b9
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-11 01:51:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,7 +31,8 @@ function updateCmdFunc(handler){
             env: { process_restarting: 1 },
             stdio: 'ignore',
           }).unref();
-          process.exit(0);
+          
+          process.exit();
     }
     );
 }
