93625edc78db2b761be89e9591dc7e56e6a4d6a8
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-13 23:36:36 +0200

    maybe fix update

--- bot.js
+++ bot.js
@@ -27,12 +27,13 @@ function updateCmdFunc(handler){
         }
         handler.textReply("Updated!");
         //restart process
+
         spawn(process.argv[0], process.argv.slice(1), {
             env: { process_restarting: 1 },
             stdio: 'ignore',
           }).unref();
           
-          process.exit();
+          setTimeout(process.exit, 6000);
     }
     );
 }
