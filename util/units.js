cc219b82dfc6dc4c0146361542ea39da98899386
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-16 10:55:05 +0200

    math

--- util/units.js
+++ util/units.js
@@ -34,7 +34,7 @@ function uselessConversion(type,value) {
 
     let possibleUnits = typeUnits.map(u => {
     return Object.entries(powers).map(power=>{
-        let powerConversion = u.convert(value)*power[1];
+        let powerConversion = u.convert(value)/power[1];
         powerConversion = Math.floor(powerConversion * 100) / 100;
         let stringName = power[0]+"-"+u.name;
         return {name:stringName,value:powerConversion};
