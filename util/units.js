702e04918e2346b88b8a1991f2b5deb13fbde282
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-16 13:05:50 +0200

    Update units.js

--- util/units.js
+++ util/units.js
@@ -24,7 +24,7 @@ class unit {
         this.baseConversion = baseConversion;
     }
     convert(value) {
-        return (value*this.baseConversion);
+        return (value/this.baseConversion);
     }
 }
 
