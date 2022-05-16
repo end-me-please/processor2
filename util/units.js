cae0a79517ca9aae4716a0c70ee8e0ef9789696c
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-16 11:02:17 +0200

    ono

--- util/units.js
+++ util/units.js
@@ -24,7 +24,7 @@ class unit {
         this.baseConversion = baseConversion;
     }
     convert(value) {
-        return this.baseConversion/value;
+        return value/this.baseConversion;
     }
 }
 
