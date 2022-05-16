08d42c76c0bf27281b3106d1b5bcdbd34813ab50
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-05-16 12:38:02 +0200

    Update units.js

--- util/units.js
+++ util/units.js
@@ -60,6 +60,7 @@ function convert(unitName,value) {
         return {name:stringName,value:powerConversion};
     })
     });
+    console.log(possibleUnits);
     possibleUnits = possibleUnits.flat(2);
     possibleUnits = possibleUnits.filter(u => { return (u.value<1600 && u.value>0.005)});    
     //select random unit
