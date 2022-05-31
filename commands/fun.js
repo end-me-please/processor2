f46a0013b8c49f18b886256d46ab7a449ca93878
end-me-please <90796271+end-me-please@users.noreply.github.com>
2022-06-01 01:29:03 +0200

    Update fun.js

--- commands/fun.js
+++ commands/fun.js
@@ -3,7 +3,7 @@ const akinator = require("discord.js-akinator");
 
 
 //Example options
-const language = "es"; //The Language of the Game
+const language = "en"; //The Language of the Game
 const childMode = false; //Whether to use Akinator's Child Mode
 const gameType = "character"; //The Type of Akinator Game to Play. ("animal", "character" or "object")
 const useButtons = true; //Whether to use Discord's Buttons
